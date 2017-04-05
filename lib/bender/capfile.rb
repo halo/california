35e8b2fbe86a5bb455ecce62e14bb0a501e4cc97
halo <github@posteo.org>
2017-04-05 11:11:24 +0200

    Satisfy capistrano SCM framework

--- lib/bender/capfile.rb
+++ lib/bender/capfile.rb
@@ -2,5 +2,9 @@ require 'pathname'
 require 'capistrano/setup'
 require 'capistrano/framework'
 
+require 'capistrano/scm/plugin'
+# No-op to tell capistrano that we don't want any SCM hooks to be loaded
+install_plugin Capistrano::SCM::Plugin
+
 load 'bender/capistrano/tasks/deploy.rake'
 load 'bender/capistrano/tasks/rails.rake'
